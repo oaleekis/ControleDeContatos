e41b0157433b6ba86a1339b77d19b690faf0140f
Alex <oalexsanderm@gmail.com>
2023-06-15 15:56:08 -0300

    required em produtos

--- ControleDeContatos/Models/ProdutoModel.cs
+++ ControleDeContatos/Models/ProdutoModel.cs
@@ -1,18 +1,27 @@
-﻿namespace ControleDeContatos.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControleDeContatos.Models
 {
     public class ProdutoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome do produto")]
         public string Nome { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal PrecoCompra { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal PrecoVenda { get; set; }
+        [Required]
         public int Quantidade { get; set; }
 
         public decimal TotalParaVenda()
         {
             return PrecoVenda * Quantidade;
         }
-
     }
 
 }
